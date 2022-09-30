8bbf4e0579f29462392ed78516ec2253711d65c1
Litong Deng <litong.deng@compass.com>
2022-09-30 11:12:20 +0800

    time.h clock()
    Signed-off-by: Litong Deng <litong.deng@compass.com>

--- _time.c
+++ _time.c
@@ -4,8 +4,10 @@
 
 #include "_time.h"
 
+#include <i386/limits.h>
 #include <printf.h>
 #include <time.h>
+#include <unistd.h>
 
 /**
  * 将给定的日历时间tm转换为以下固定的25个字符格式的文本表示形式：
@@ -36,4 +38,20 @@ void show_asctime() {
   printf("%s\n", str);
 }
 
-void show_time() { show_asctime(); }
+void show_clock() {
+  clock_t start, end;
+  start = clock();  // 获取处理器时钟
+  double a = 3.14;
+  for (int i = 0; i < 1000; ++i) {
+    a *= 2;
+  }
+  printf("a = %lf\n", a);
+  // sleep(5);  // can not affect clock(), why?
+  end = clock();
+  printf("Time consumed: %lf\n", (end - start) * 1.0 / CLK_TCK);
+}
+
+void show_time() {
+  // show_asctime();
+  show_clock();
+}
